04df55f09b21c84f2f1a8fe4ab2663c01dc1321e
haofu <hfu1017@gmail.com>
2020-05-22 16:43:06 -0700

    First commit

---
+++ service/app/controller/default/home.js
@@ -0,0 +1,93 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable comma-dangle */
+/* eslint-disable key-spacing */
+/* eslint-disable spaced-comment */
+/* eslint-disable comma-spacing */
+/* eslint-disable space-infix-ops */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+/* eslint-disable prefer-const */
+/* eslint-disable indent */
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+    // eslint-disable-next-line space-before-blocks
+    async index(){
+        //获取用户表的数据
+
+        this.ctx.body= 'api'
+    }
+    async getArticleList() {
+
+        let sql = 'SELECT article.id as id,'+
+                  'article.title as title,'+
+                  'article.introduce as introduce,'+
+                  "FROM_UNIXTIME(article.add_time,'%Y-%m-%d' ) as add_time," +
+                  //"DATE_FORMAT(article.add_time, '%Y-%m-%d') as add_time," +
+                  'article.view_count as view_count ,'+
+                  '.type.typeName as typeName '+
+                  'FROM article LEFT JOIN type ON article.type_id = type.id'
+     
+         const results = await this.app.mysql.query(sql)
+     
+         this.ctx.body={
+             data:results
+         }
+     }
+
+     async getArticleById() {
+        //先配置路由的动态传值，然后再接收值
+        let id = this.ctx.params.id
+
+        let sql = 'SELECT article.id as id,'+
+        'article.title as title,'+
+        'article.introduce as introduce,'+
+        'article.article_content as article_content,'+
+        "FROM_UNIXTIME(article.add_time,'%Y-%m-%d' ) as add_time," +
+        'article.view_count as view_count ,'+
+        'type.typeName as typeName ,'+
+        'type.id as type_id '+
+        'FROM article LEFT JOIN type ON article.type_id = type.id '+
+        'WHERE article.id='+id
+        
+        const result = await this.app.mysql.query(sql)
+
+
+        this.ctx.body={ data:result }
+
+    }
+
+    // get type name and number
+    async getTypeInfo(){
+
+        const result = await this.app.mysql.select('type')
+        this.ctx.body = {data:result}
+  
+    }
+
+    // get list by id
+    async getListById(){
+
+        let id = this.ctx.params.id
+        let sql = 'SELECT article.id as id,'+
+        'article.title as title,'+
+        'article.introduce as introduce,'+
+        "FROM_UNIXTIME(article.add_time,'%Y-%m-%d' ) as add_time," +
+        'article.view_count as view_count,'+
+        'type.typeName as typeName,'+
+        'type.id as type_id '+
+        'FROM article LEFT JOIN type ON article.type_id = type.id ' +
+        'WHERE type_id='+id
+     
+        const results = await this.app.mysql.query(sql)
+     
+        this.ctx.body={
+            data:results
+        }
+  
+    }
+}
+
+module.exports = HomeController;
